9ff6ca946cf3ff7c9ec5ddebf011b36b16ce1461
rimuru2003 <karansnh007@gmail.com>
2024-07-15 22:43:51 +0530

    imp

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,23 @@
 import React, { useEffect, useState } from "react";
 import "../css/Home.css";
+import { FaInstagram } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+import { SiGmail } from "react-icons/si";
+import { Link } from "react-router-dom";
+
+
+
+import { FaLinkedin } from "react-icons/fa";
 
 const Home = () => {
   const [para, setPara] = useState("");
   const [w, setW] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
 
   const name = "Hello, I'm Aryan Singh";
   const work = {
@@ -23,7 +37,7 @@ const Home = () => {
       const timers = setTimeout(textapp, speed);
       return () => clearTimeout(timers);
     }
-  });
+  }, [w, name.length]);
 
   return (
     <div className="ho1">
@@ -31,10 +45,53 @@ const Home = () => {
         <h1 className="ho3">{para}</h1>
         <h2 className="ho4">{work.work}</h2>
         <p className="ho5">{work.desc}</p>
-        <button className="ho8" >Contact <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-         <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-         </svg>
+        <div className={`menu ${isActive ? "active" : ""}`}>
+          <button className={`toggle ${isActive ? "active" : ""}`} onClick={toggleMenu}>
+            Contact
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 12L3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+              />
+            </svg>
+            <ul>
+              <li style={{ "--i": 1, "--clo": "#c32aa3" }}>
+                <Link to="https://www.instagram.com/aryansingh070503/?hl=en">
+                  <FaInstagram />
+                </Link>
+              </li>
+              <li style={{ "--i": 2, "--clo": "#000000" }}>
+                <Link to="https://x.com/karansnh007">
+                  <FaSquareXTwitter />
+                </Link>
+              </li>
+              <li style={{ "--i": 3, "--clo": "#BB001B" }}>
+                <Link to="mailto:karansnh007@@gmail.com">
+                <SiGmail />
+
+                </Link>
+              </li>
+              <li style={{ "--i": 4, "--clo": "#000000" }}>
+                <Link to="https://github.com/rimuru2003">
+                  <FaGithub />
+                </Link>
+              </li>
+              <li style={{ "--i": 5, "--clo": "#0a66c2" }}>
+                <Link to="https://www.linkedin.com/in/aryan-singh-a57715224/">
+                  <FaLinkedin />
+                </Link>
+              </li>
+            </ul>
           </button>
+        </div>
       </div>
       <span className="ho6">
         <img
@@ -43,7 +100,6 @@ const Home = () => {
           alt="image"
         />
       </span>
-      
     </div>
   );
 };
--- src/component/Project.js
+++ src/component/Project.js
@@ -1,28 +1,42 @@
 import React from "react";
 import "../css/Project.css";
+import Project1 from "../pic/Project1.png";
+import Project2 from "../pic/Project2.png";
+import Project3 from "../pic/Project3.png";
 
 const Project = () => {
   return (
     <>
-    <h1 className="head" >A small selection of recent projects
-    </h1>
-    <div className="p1">
-     
-      <div className="p">
-        <div className="front-face"></div>
-        <div className="back-face"></div>
+      <h1 className="head">A small selection of recent projects</h1>
+      <div className="p1">
+        <div className="p">
+          <div className="front-face">
+            <img className="project1" src={Project1} alt="Project 1" />
+          </div>
+          <div className="back-face">
+            <h1>Project 1 Details</h1>
+            <p>Description of Project 1</p>
+          </div>
+        </div>
+        <div className="p">
+          <div className="front-face">
+            <img className="project1" src={Project2} alt="Project 2" />
+          </div>
+          <div className="back-face">
+            <h1>Project 2 Details</h1>
+            <p>Description of Project 2</p>
+          </div>
+        </div>
+        <div className="p">
+          <div className="front-face">
+            <img className="project1" src={Project3} alt="Project 3" />
+          </div>
+          <div className="back-face">
+            <h1>Project 3 Details</h1>
+            <p>Description of Project 3</p>
+          </div>
+        </div>
       </div>
-      <div className="p">
-        <div className="front-face"></div>
-        <div className="back-face"></div>
-      </div>
-      <div className="p">
-        <div className="front-face"></div>
-        <div className="back-face"></div>
-      </div>
-      
-      
-    </div>
     </>
   );
 };
