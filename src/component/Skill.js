e72bcf7881a11ff5e8dc900c5e1bbfdd17023095
rimuru2003 <karansnh007@gmail.com>
2024-07-16 16:56:18 +0530

    improve

--- src/component/Skill.js
+++ src/component/Skill.js
@@ -8,35 +8,40 @@ const Skill = () => {
       <div className="so1">
         <h1 className="skillheading">Stack</h1>
         <div className="so2">
+        <i className="devicon-javascript-plain icon"></i>
+
+        <i class="devicon-nextjs-plain icon"></i>
+                    <i class="devicon-redux-original icon"></i>
           <i className="devicon-html5-plain colored icon"></i>
-          <i className="devicon-mongodb-plain colored icon"></i>
-          <i className="devicon-javascript-plain icon"></i>
-          <i className="devicon-java-plain colored icon"></i>
-          <i className="devicon-vercel-original colored icon"></i>
-          <i className="devicon-react-original colored icon"></i>
-          <i className="devicon-nextjs-plain icon"></i>
-          <i className="devicon-redux-original colored icon"></i>
+          <i className="devicon-css3-plain colored icon"></i>
+          <i className="devicon-tailwindcss-original colored icon"></i>
+          
           <i className="devicon-github-original colored icon"></i>
           <i className="devicon-linux-plain colored icon"></i>
-          <i className="devicon-firebase-plain colored icon"></i>
-          <i className="devicon-cplusplus-plain colored icon"></i>
-          <i className="devicon-archlinux-plain colored icon"></i>
-          <i className="devicon-tailwindcss-original colored icon"></i>
-          <i className="devicon-netlify-plain colored icon"></i>
-          <i className="devicon-css3-plain colored icon"></i>
-          <i class="devicon-nodejs-plain-wordmark colored"></i>
+         
+          <i class="devicon-cplusplus-plain icon"></i>
+          <i className="devicon-mongodb-plain colored icon"></i>
+          <i className="devicon-java-plain colored icon"></i>
+         
+           <i className="devicon-firebase-plain colored icon"></i> 
+           <i class="devicon-c-line icon"></i>
+          
+          
+          
+          <i class="devicon-reactrouter-plain icon"></i>
 
-          <i class="devicon-git-plain"></i>
+
+          <i class="devicon-express-original icon"></i>
+                    <i className="devicon-react-original colored icon"></i>
+          <i className="devicon-archlinux-plain colored icon"></i>
+          
+          <i class="devicon-nodejs-plain-wordmark colored icon"></i>
+
+     
           
           
          
-          <i class="devicon-c-line"></i>
-          
-          
-          
-          <i class="devicon-reactrouter-plain"></i>
-
-          <i class="devicon-express-original"></i>
+        
         </div>
       </div>
     </>
