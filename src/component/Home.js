235eba156823a6fe4fc4034585c44e07e63b3a9f
rimuru2003 <karansnh007@gmail.com>
2024-07-20 18:34:06 +0530

    firebase and redux

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,52 +1,58 @@
 import React, { useEffect, useState } from "react";
 import "../css/Home.css";
-import { FaInstagram } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaGithub } from "react-icons/fa6";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter, FaGithub } from "react-icons/fa6";
 import { SiGmail } from "react-icons/si";
-import { Link } from "react-router-dom";
-
-
-
-import { FaLinkedin } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const Home = () => {
   const [para, setPara] = useState("");
   const [w, setW] = useState(0);
   const [isActive, setIsActive] = useState(false);
 
+  const items = useSelector((state) => state.counter.items);
+
   const toggleMenu = () => {
     setIsActive(!isActive);
   };
-
-  const name = "Hello, I'm Aryan Singh";
-  const work = {
-    work: "Frontend Developer,",
-    desc: "I love crafting solid and scalable front-end products with great user experiences. Skilled in HTML, CSS, and JavaScript, I thrive on creating intuitive and visually appealing websites. My goal is to build seamless and engaging web applications that delight users.",
-  };
+  
+  const name = items.home?.find((item) => item.nametag)?.nametag || "";
+  const work = items.home?.find((item) => item.work)?.work || "";
+  const desc = items.home?.find((item) => item.desc)?.desc || "";
+  
+  
   const speed = 50;
-
+  // const connect = items.link?.find((item) => item.link)?.link || "";
+  
+  const link = [
+    { href: "https://www.instagram.com/aryansingh070503/?hl=en", icon: <FaInstagram />, color: "#c32aa3", id: 1 },
+    { href: "https://x.com/karansnh007", icon: <FaSquareXTwitter />, color: "#000000", id: 2 },
+    { href: "mailto:karansnh007@gmail.com", icon: <SiGmail />, color: "#BB001B", id: 3 },
+    { href: "https://github.com/rimuru2003", icon: <FaGithub />, color: "#000000", id: 4 },
+    { href: "https://www.linkedin.com/in/aryan-singh-a57715224/", icon: <FaLinkedin />, color: "#0a66c2", id: 5 }
+  ];
   useEffect(() => {
-    const textapp = () => {
-      if (w < name.length) {
-        setPara((a) => a + name.charAt(w));
-        setW((prevI) => prevI + 1);
-      }
-    };
     if (w < name.length) {
-      const timers = setTimeout(textapp, speed);
-      return () => clearTimeout(timers);
+      const textapp = () => {
+        setPara((a) => `${a}${name.charAt(w)}`);
+        setW((prevI) => prevI + 1);
+      };
+      const timer = setTimeout(textapp, speed);
+      return () => clearTimeout(timer);
     }
-  }, [w, name.length]);
+  }, [w, name]);
 
   return (
     <div className="ho1">
       <div className="ho2">
         <h1 className="ho3">{para}</h1>
-        <h2 className="ho4">{work.work}</h2>
-        <p className="ho5">{work.desc}</p>
+        <h2 className="ho4">{work}</h2>
+        <p className="ho5">{desc}</p>
         <div className={`menu ${isActive ? "active" : ""}`}>
-          <button className={`toggle ${isActive ? "active" : ""}`} onClick={toggleMenu}>
+          <button
+            className={`toggle ${isActive ? "active" : ""}`}
+            onClick={toggleMenu}
+          >
             Contact
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -62,33 +68,16 @@ const Home = () => {
                 d="M6 12L3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
               />
             </svg>
-            <ul>
-              <li style={{ "--i": 1, "--clo": "#c32aa3" }}>
-                <Link to="https://www.instagram.com/aryansingh070503/?hl=en">
-                  <FaInstagram />
-                </Link>
-              </li>
-              <li style={{ "--i": 2, "--clo": "#000000" }}>
-                <Link to="https://x.com/karansnh007">
-                  <FaSquareXTwitter />
-                </Link>
-              </li>
-              <li style={{ "--i": 3, "--clo": "#BB001B" }}>
-                <Link to="mailto:karansnh007@@gmail.com">
-                <SiGmail />
 
-                </Link>
-              </li>
-              <li style={{ "--i": 4, "--clo": "#000000" }}>
-                <Link to="https://github.com/rimuru2003">
-                  <FaGithub />
-                </Link>
-              </li>
-              <li style={{ "--i": 5, "--clo": "#0a66c2" }}>
-                <Link to="https://www.linkedin.com/in/aryan-singh-a57715224/">
-                  <FaLinkedin />
-                </Link>
-              </li>
+           
+            <ul>
+            {link?.map(({ href, icon, color, id }) => (
+                <li key={id} style={{ "--i": id, "--clo": color }}>
+                  <a href={href} target="_blank" rel="noopener noreferrer">
+                    {icon}
+                  </a>
+                </li>
+              ))}
             </ul>
           </button>
         </div>
