0c45035eef5fa3f44b984b7daf44db3c7f25fa33
rimuru2003 <karansnh007@gmail.com>
2024-07-21 01:27:27 +0530

    css

--- src/component/About.js
+++ src/component/About.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../css/About.css";
-import Aboutimg from '../pic/Aboutimg.svg';
+
 import { useSelector } from "react-redux";
 
 const About = () => {
-  const items = useSelector((state) => state.counter.items);
-  const about = items.info?.find(item => item.aboutMe)?.aboutMe || "";
 
+  const [abimg ,setAbimg] = useState(null);
+  const [about ,setAbout] = useState(null);
+
+
+  const items = useSelector((state) => state.counter.items);
+  const abouts = items.info?.find(item => item.aboutMe)?.aboutMe || "";
+  const aboutimg = items.img?.find(item => item.about)?.about || "";
+
+useEffect(() =>{
+  setAbimg(aboutimg)
+  setAbout(abouts)
+})
   
 
   return (
     <>
       <div className="a1">
-        <img className="a2" src={Aboutimg} alt="About" />
+        <img className="a2" src={abimg} alt="About" />
         <div className="a4">
           <h1 className="a7">About Me</h1>
           <p className="a6">{about}</p>
--- src/component/Project.js
+++ src/component/Project.js
@@ -1,55 +1,92 @@
-import React from "react";
+import React,{useRef} from "react";
 import "../css/Project.css";
 import Project1 from "../pic/Project1.png";
 import Project2 from "../pic/Project2.png";
 import Project3 from "../pic/Project3.png";
+import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+import { MdOutlineKeyboardDoubleArrowLeft } from "react-icons/md";
 
 const Project = () => {
+  const scrollRef = useRef(null); // Create a reference for the scrollable container
+
+  // Function to scroll the container to the left
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft -= 400; // Scroll left by 4000 pixels
+    }
+  };
+
+  // Function to scroll the container to the right  
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft += 400; // Scroll right by 400 pixels
+    }
+  };
+
   return (
     <>
-      <h1 className="head">A small selection </h1>
+      <h1 className="head">A small selection</h1>
       <h1 className="heads">of recent projects</h1>
-      <div className="p1">
-       
-        <div className="p">
-          <div className="p2">
-          <div className="front-face">
-            <img className="project1" src={Project1} alt="Project 1" />
-          </div>
-          <div className="back-face">
-          {/* <img className="project2" src={Project1} alt="Project 1" /> */}
-
-            <h1>Project 1 Details</h1>
-            <p>Description of Project 1</p>
+      <div className="projects-container" ref={scrollRef}>
+        <div className="project">
+          <div className="hide">
+            <div className="project-inner">
+              <div className="front-face">
+                <img className="project-image" src={Project1} alt="Project 1" />
+              </div>
+              <div className="back-face">
+                <h1>Project 1 Details</h1>
+                <p>Description of Project 1</p>
+              </div>
             </div>
           </div>
         </div>
-        
-        <div className="p">
-          <div className="p2">
-          <div className="front-face">
-            <img className="project1" src={Project2} alt="Project 2" />
-          </div>
-          <div className="back-face">
-            <h1>Project 2 Details</h1>
-            <p>Description of Project 2</p>
-          </div>
+        <div className="project">
+          <div className="hide">
+            <div className="project-inner">
+              <div className="front-face">
+                <img className="project-image" src={Project2} alt="Project 2" />
+              </div>
+              <div className="back-face">
+                <h1>Project 2 Details</h1>
+                <p>Description of Project 2</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="p">
-        <div className="p2">
-
-          <div className="front-face">
-            <img className="project1" src={Project3} alt="Project 3" />
+        <div className="project">
+          <div className="hide">
+            <div className="project-inner">
+              <div className="front-face">
+                <img className="project-image" src={Project3} alt="Project 3" />
+              </div>
+              <div className="back-face">
+                <h1>Project 3 Details</h1>
+                <p>Description of Project 3</p>
+              </div>
+            </div>
           </div>
-          <div className="back-face">
-            <h1>Project 3 Details</h1>
-            <p>Description of Project 3</p>
-          </div>
+        </div>
+        <div className="project">
+          <div className="hide">
+            <div className="project-inner">
+              <div className="front-face">
+                <img className="project-image" src={Project3} alt="Project 4" />
+              </div>
+              <div className="back-face">
+                <h1>Project 4 Details</h1>
+                <p>Description of Project 4</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    
+      <button id="prev" onClick={scrollLeft}>
+        <MdOutlineKeyboardDoubleArrowLeft />
+      </button>
+      <button id="next" onClick={scrollRight}>
+        <MdOutlineKeyboardDoubleArrowRight />
+      </button>
     </>
   );
 };
