c8393d0a0b143049e320bf1f81fdfaedba3fa940
rimuru2003 <karansnh007@gmail.com>
2024-09-09 13:39:39 +0530

    .

--- src/component/Skill.jsx
+++ src/component/Skill.jsx
@@ -1,63 +1,61 @@
-// Skill.js
 import React, { useEffect, useState } from "react";
 import "../css/Skill.css";
 import { useSelector } from "react-redux";
 
 const Skill = () => {
-  const [client, setClient] = useState(null);
-  const [server, setServer] = useState(null);
-  const [dbms, setDbms] = useState(null);
-  const [prog, setProg] = useState(null);
-  const [dev, setDev] = useState(null);
-  const [version, setVersion] = useState(null);
-
-  const ClientSide = useSelector((state) => state.counter.items?.ClientSide);
-  const DatastoringandORMs = useSelector(
-    (state) => state.counter.items?.DatastoringandORMs
-  );
-  const DevOpsandDevelopmentTools = useSelector(
-    (state) => state.counter.items?.DevOpsandDevelopmentTools
-  );
-  const ProgrammingLanguages = useSelector(
-    (state) => state.counter.items?.ProgrammingLanguages
-  );
-  const ServerSide = useSelector((state) => state.counter.items?.ServerSide);
-  const VersionControlandCollaboration = useSelector(
-    (state) => state.counter.items?.VersionControlandCollaboration
-  );
+  const [skills, setSkills] = useState({});
+  const stacks = useSelector((state) => state.counter.items?.skill);
 
   useEffect(() => {
-    setClient(ClientSide || []);
-    setServer(ServerSide || []);
-    setDbms(DatastoringandORMs || []);
-    setProg(ProgrammingLanguages || []);
-    setDev(DevOpsandDevelopmentTools || []);
-    setVersion(VersionControlandCollaboration || []);
-  }, [
-    ClientSide,
-    VersionControlandCollaboration,
-    ServerSide,
-    DatastoringandORMs,
-    ProgrammingLanguages,
-    DevOpsandDevelopmentTools,
-  ]);
+    setSkills(stacks || {});
+  }, [stacks]);
 
   return (
     <>
       <div className="so1">
         <h1 className="skillheading">Stack</h1>
         <div className="so2">
-          <div className="parent">
-              <h1 className="skillname">Client Side</h1>
-            <div className="so3">
-              {client?.map((cl, i) => (
-                <i key={i} className={`${cl} icons`}></i>
-              ))}
-            </div>
-          </div>
-          <div className="parent">
-              <h1 className="skillname">Server Side</h1>
-            <div className="so3">
+          {Object.keys(skills).map((category, index) => {
+            const skillList = skills[category];
+            const categoryName = skillList.find((item) => item?.name)?.name || category;
+
+            return (
+              <div key={index} className="parent">
+                <h1 className="skillname">{categoryName}</h1>
+                <div className="so3">
+                  {skillList
+                    .map((skill, i) => (
+                      <i key={i} className={`${skill} icons`}></i>
+                    ))}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Skill;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="parent">
+          <h1 className="skillname">{server?.find(c => typeof c === "object")?.name || "Client Side"}</h1>
+          <div className="so3">
 
               {server?.map((sl, i) => (
                 <i key={i} className={`${sl} icons`}></i>
@@ -65,8 +63,8 @@ const Skill = () => {
             </div>
           </div>
           <div className="parent">
-              <h1 className="skillname">Programming Languages</h1>
-            <div className="so3">
+          <h1 className="skillname">{prog?.find(c => typeof c === "object")?.name || "Client Side"}</h1>
+          <div className="so3">
 
               {prog?.map((pl, i) => (
                 <i key={i} className={`${pl} icons`}></i>
@@ -74,8 +72,8 @@ const Skill = () => {
             </div>
           </div>
           <div className="parent">
-              <h1 className="skillname">Data storing and ORMs</h1>
-            <div className="so3">
+          <h1 className="skillname">{dbms?.find(c => typeof c === "object")?.name || "Client Side"}</h1>
+          <div className="so3">
 
               {dbms?.map((dl, i) => (
                 <i key={i} className={`${dl} icons`}></i>
@@ -84,8 +82,8 @@ const Skill = () => {
           </div>
 
           <div className="parent">
-              <h1 className="skillname">Development Tools</h1>
-            <div className="so3">
+          <h1 className="skillname">{dev?.find(c => typeof c === "object")?.name || "Client Side"}</h1>
+          <div className="so3">
 
               {dev?.map((del, i) => (
                 <i key={i} className={`${del} icons`}></i>
@@ -94,18 +92,57 @@ const Skill = () => {
           </div>
 
           <div className="parent">
-              <h1 className="skillname">Version Control & Collaboration</h1>
-            <div className="so3">
+          <h1 className="skillname">{version?.find(c => typeof c === "object")?.name || "Client Side"}</h1>
+          <div className="so3">
 
               {version?.map((vl, i) => (
                 <i key={i} className={`${vl} icons`}></i>
               ))}
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+          </div> */}
 
-export default Skill;
+
+
+
+
+
+
+
+
+            // const [client, setClient] = useState(null);
+  // const [server, setServer] = useState(null);
+  // const [dbms, setDbms] = useState(null);
+  // const [prog, setProg] = useState(null);
+  // const [dev, setDev] = useState(null);
+  // const [version, setVersion] = useState(null);
+
+  // const ClientSide = useSelector((state) => state.counter.items?.ClientSide);
+  // const DatastoringandORMs = useSelector(
+  //   (state) => state.counter.items?.DatastoringandORMs
+  // );
+  // const DevOpsandDevelopmentTools = useSelector(
+  //   (state) => state.counter.items?.DevOpsandDevelopmentTools
+  // );
+  // const ProgrammingLanguages = useSelector(
+  //   (state) => state.counter.items?.ProgrammingLanguages
+  // );
+  // const ServerSide = useSelector((state) => state.counter.items?.ServerSide);
+  // const VersionControlandCollaboration = useSelector(
+  //   (state) => state.counter.items?.VersionControlandCollaboration
+  // );
+
+  // useEffect(() => {
+  //   setClient(ClientSide || []);
+  //   setServer(ServerSide || []);
+  //   setDbms(DatastoringandORMs || []);
+  //   setProg(ProgrammingLanguages || []);
+  //   setDev(DevOpsandDevelopmentTools || []);
+  //   setVersion(VersionControlandCollaboration || []);
+  // }, [
+  //   ClientSide,
+  //   VersionControlandCollaboration,
+  //   ServerSide,
+  //   DatastoringandORMs,
+  //   ProgrammingLanguages,
+  //   DevOpsandDevelopmentTools,
+  // ]);
