ac01ae7ded6f92eabd2c532672a590fce2716c51
rimuru2003 <karansnh007@gmail.com>
2024-09-21 13:07:21 +0530

    .

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC34dWEAQ9K4-_siurtbYlA97YvMICVa9o",
-  authDomain: "portfolio-e8312.firebaseapp.com",
-  projectId: "portfolio-e8312",
-  storageBucket: "portfolio-e8312.appspot.com",
-  messagingSenderId: "562785410485",
-  appId: "1:562785410485:web:6b418deb289651b9accdfc",
-  measurementId: "G-N9RF65HK2K"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
